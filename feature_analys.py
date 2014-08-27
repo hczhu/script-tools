24945dba457ca6b64f5210e76c11497d17f770dc
hcz <hcz@fb.com>
2014-08-27 17:21:58 +0100

    Minor changes.

--- feature_analys.py
+++ feature_analys.py
@@ -31,42 +31,42 @@ def HellingerDistance(mean1, sigma1, mean2, sigma2):
   return math.sqrt(1 - math.sqrt(2.0 * sigma1 * sigma2 / (sigma1 * sigma1 + sigma2 * sigma2)) *
         math.exp(-math.pow(mean1 - mean2, 2) / (4 * (sigma1 * sigma1 + sigma2 * sigma2))))
 
-names = sys.stdin.readline().strip().split(',')
-matrix = []
-for line in sys.stdin:
-  features = [0.0] * (len(names) + 1)
-  tokens = line.strip().split(',')
-  for idx in range(len(tokens)):
-    if -1 == tokens[idx].find(':'):
-      features[idx] = float(tokens[idx])
-    else:
-      fid, value = tokens[idx].split(':')
-      features[int(fid)] = float(value)
-  matrix.append(features)
-
-matrix = numpy.matrix(matrix)
-
-header = ['Feature', 'Positive Mean(Std Dev)', 'Negative Mean(Std Dev)', 'Hellinger distance', 'Correlation']
-
-table_content = []
-positive_index = numpy.nonzero(matrix[:, 0])[0].tolist()[0]
-negative_index = list(set(range(matrix.shape[0])) - set(positive_index))
-for idx in range(len(names)):
-  line = [names[idx]]
-  labels = numpy.transpose(matrix[:, 0].tolist())[0]
-  values = numpy.transpose(matrix[:, idx + 1].tolist())[0]
-  positive = matrix[positive_index, [idx + 1]][0]
-  negative = matrix[negative_index, [idx + 1]][0]
-  pm, pd = positive.mean(), positive.std()
-  nm, nd = negative.mean(), negative.std()
-  table_content.append([
-           names[idx],
-           '%.3f(%.3f)'%(positive.mean(), positive.std()),
-           '%.3f(%.3f)'%(negative.mean(), negative.std()),
-           HellingerDistance(pm, pd, nm, nd),
-           Pearson(labels, values),
-         ])
+if __name__ == "__main__":
+  names = sys.stdin.readline().strip().split(',')
+  matrix = []
+  for line in sys.stdin:
+    features = [0.0] * (len(names) + 1)
+    tokens = line.strip().split(',')
+    for idx in range(len(tokens)):
+      if -1 == tokens[idx].find(':'):
+        features[idx] = float(tokens[idx])
+      else:
+        fid, value = tokens[idx].split(':')
+        features[int(fid)] = float(value)
+    matrix.append(features)
   
-
-table_content.sort(reverse = True, key = lambda line: abs(line[4]))
-PrintTable(header, table_content)
+  matrix = numpy.matrix(matrix)
+  
+  header = ['Feature', 'Positive Mean(Std Dev)', 'Negative Mean(Std Dev)', 'Hellinger distance', 'Correlation']
+  
+  table_content = []
+  positive_index = numpy.nonzero(matrix[:, 0])[0].tolist()[0]
+  negative_index = list(set(range(matrix.shape[0])) - set(positive_index))
+  for idx in range(len(names)):
+    line = [names[idx]]
+    labels = numpy.transpose(matrix[:, 0].tolist())[0]
+    values = numpy.transpose(matrix[:, idx + 1].tolist())[0]
+    positive = matrix[positive_index, [idx + 1]][0]
+    negative = matrix[negative_index, [idx + 1]][0]
+    pm, pd = positive.mean(), positive.std()
+    nm, nd = negative.mean(), negative.std()
+    table_content.append([
+             names[idx],
+             '%.3f(%.3f)'%(pm, pd),
+             '%.3f(%.3f)'%(nm, nd),
+             HellingerDistance(pm, pd, nm, nd),
+             Pearson(labels, values),
+           ])
+  
+  table_content.sort(reverse = True, key = lambda line: abs(line[4]))
+  PrintTable(header, table_content)

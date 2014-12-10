43f82860a4ef5739f146f8ba4ac2d5fb6ab64b11
Hongcheng Zhu <zhuhcheng@gmail.com>
2014-12-09 23:47:58 -0800

    update strategies.

--- table_printer.py
+++ table_printer.py
@@ -9,7 +9,7 @@ def PrintOneLine(table_header, col_len, silent_column = []):
     line += '-' * col_len[i] + '|'
   return line
 
-def PrintTable(table_header, records, silent_column = [], truncate_float = True):
+def PrintTable(table_header, records, silent_column = [], truncate_float = True, header_transformer = lambda name: name):
   silent_column = set(silent_column)
   col_len = [0] * len(table_header)
   for cells in records:
@@ -19,8 +19,8 @@ def PrintTable(table_header, records, silent_column = [], truncate_float = True)
       col_len[i] = max(col_len[i], len(str(cells[i])))
   for i in range(len(col_len)):
     if col_len[i] > 0:
-      col_len[i] = max(col_len[i], len(table_header[i]))
-  line = PrintOneLine(table_header, col_len, silent_column)
+      col_len[i] = max(col_len[i], len(header_transformer(table_header[i])))
+  line = PrintOneLine(map(header_transformer, table_header), col_len, silent_column)
   header = '+' + line[1:len(line) - 1] + '+'
   print header
   records.insert(0, table_header)
@@ -38,8 +38,8 @@ def PrintTable(table_header, records, silent_column = [], truncate_float = True)
   print header
 
 # 'records_map' are an array of map.
-def PrintTableMap(table_header, records_map, silent_column = [], truncate_float = True):
+def PrintTableMap(table_header, records_map, silent_column = [], truncate_float = True, header_transformer = lambda name: name):
   records = []
   for r in records_map:
     records.append([r.get(col, '') for col in table_header])
-  PrintTable(table_header, records, silent_column, truncate_float)
+  PrintTable(table_header, records, silent_column, truncate_float, header_transformer)
